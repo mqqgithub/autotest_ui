ddae194c9c0ca74e144a78e76c0cd845459835cc
mqqgithub <maqq1234>
2020-05-03 22:24:47 +0800

    123

---
+++ common/get_data.py
@@ -0,0 +1,66 @@
+'''
+获取数据
+'''
+import xlrd, csv
+
+txt_path = "./data1.txt"
+xls_path = "./data2.xls"
+csv_path = "./data3.csv"
+
+
+class GetData():
+
+    def __init__(self):
+        pass
+
+    def txt_data(self, path):
+        with open(path, 'r+', encoding='utf-8') as f:
+            s = [i[:-1].split(',') for i in f.readlines()]  # 最后一行要回车
+            print(s)
+            return s
+
+    def excel_data(self, path):
+        # 打开execl
+        workbook = xlrd.open_workbook(path)
+
+        # 输出Excel文件中所有sheet的名字
+        # print(workbook.sheet_names())
+
+        # 根据sheet索引或者名称获取sheet内容
+        Data_sheet = workbook.sheets()[0]  # 通过索引获取
+        # Data_sheet = workbook.sheet_by_index(0)  # 通过索引获取
+        # Data_sheet = workbook.sheet_by_name(u'名称')  # 通过名称获取
+
+        # print(Data_sheet.name)  # 获取sheet名称
+        rowNum = Data_sheet.nrows  # sheet行数
+        colNum = Data_sheet.ncols  # sheet列数
+
+        # 获取所有单元格的内容
+        lis = []
+        for i in range(rowNum):
+            rowlist = []
+            for j in range(colNum):
+                rowlist.append(Data_sheet.cell_value(i, j))
+            lis.append(rowlist)
+        # print(lis)
+        return lis
+
+    def csv_data(self, path):
+        lis = []
+        with open(path, "r", encoding='gbk') as csvfile:
+            reader = csv.reader(csvfile)
+            for line in reader:
+                lis.append(line)
+        print(lis)
+        return lis
+
+    def mysql_data(self):
+        pass
+
+
+if __name__ == "__main__":
+    data = GetData()
+    txt = data.txt_data(txt_path)
+    #excel = data.excel_data()
+    #csv = data.csv_data()
+    #mysql = data.mysql_data()
---
+++ test_case/test_data.py
@@ -0,0 +1,36 @@
+# 数据驱动测试，首先需要安装DDT包
+from ddt import ddt, data, unpack
+from common.get_data import GetData
+import unittest
+
+test_data = GetData().txt_data(r'D:/autotest_ui/common/data1.txt')
+# print(test_data)
+
+# 使用前必须先声明
+@ddt
+class Test(unittest.TestCase):
+    def setUp(self):
+        print("test start...")
+
+    def tearDown(self):
+        print("test over...")
+
+    @data('aa', 'bb')  # 单个参数分2次运行用例
+    def test_1(self, s):
+        print("test1%s" % s)
+
+    @data((1, 1), (2, 2))  # 多个参数必须要拆包,不然会作为一个参数
+    @unpack
+    def test_2(self, x, y):
+        print("test2%d" % (x+y))
+
+    @data(*test_data)  # data的数据必须是元祖，加*是把数据变成元祖类型  test_data 可以是函数*test_data()返回值
+    @unpack
+    def test_3(self, a, b ,c):
+        print(a)
+        print(b)
+        print(c)
+
+
+if __name__ == '__main__':
+    unittest.main()
